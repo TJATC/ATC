10e232d5b72fda538ad35386302e026a3d0b5214
Hael <leahZhang1595995@gmail.com>
2024-05-05 00:38:49 -0400

    Wait

--- src/app/about_us/page.tsx
+++ src/app/about_us/page.tsx
@@ -1,6 +1,8 @@
+import Hero from "@/(components)/Hero"
 export default function about(){
     return(
         <div>
+            <Hero source="/about_us.png"/>
             <p>about us </p>
         </div>
     )
