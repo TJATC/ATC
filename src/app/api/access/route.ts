cca179c607e4cc2ab9970413aad9ae9dddc8851a
Leah Zhang <LeahZhang1595995@gmail.com>
2024-06-01 23:45:56 -0400

    k

--- src/app/api/access/route.ts
+++ src/app/api/access/route.ts
@@ -3,10 +3,10 @@ import { NextRequest, NextResponse } from 'next/server'
 
 export async function POST(request: NextRequest){
     const req = await request.json()
-    console.log(req)
     try {
         const cookieStore = cookies()
         const token = cookieStore.get(req.token)
+        console.log(token)
         return NextResponse.json({token:token}, {status:200})
     } catch (error) {
         
