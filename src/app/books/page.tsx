cd996e9a61066a6657ff1a42f6c2ff3639233f56
Leah Zhang <LeahZhang1595995@gmail.com>
2024-06-07 16:14:02 -0400

    fixed Books

--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -2,17 +2,24 @@
 import axios from "axios";
 import Box from "./Box"
 import "./Books.css"
+import { useEffect } from "react";
 
 export default async function books(){
-    const onLogin = async () =>{
-        try {
-            const response = await axios.post('/api/slides')
-            return response.data.data
-        } catch (error:any) {
-            console.log(error.message)
+    let data:any = {}
+    useEffect(()=>{
+        const onLogin = async () =>{
+            try {
+                const response = await axios.post('/api/slides')
+                return response.data.data
+            } catch (error:any) {
+                console.log(error.message)
+            }
         }
-    }
-    const data:any = await onLogin()
+        data = onLogin()
+    }, [])
+    
+
+    
     console.log(data)
     return(
         <div className={["Books"].join(" ")}>
