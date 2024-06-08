dc5c4601cab28c4a973e9444bd2e6bc8a940946d
Leah Zhang <LeahZhang1595995@gmail.com>
2024-06-08 01:25:13 -0400

    d

--- src/app/about_us/page.tsx
+++ src/app/about_us/page.tsx
@@ -9,9 +9,11 @@ export default function about(){
             <Main source="/about_us.png"/>
             
             <Heading link="none" num="1" blurb="Our Approach"/>
-            {/*
+            
             <Approach/>
+            
             <Heading link="none" num="2" blurb="Leadership"/>
+            {/*
             <Leadership/>
             <Heading link="none" num="3" blurb="History"/> */}
         </div>
